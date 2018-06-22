764f196c2ba98f7cd6dd5187e5959b179534caad
Henrik Erhart <erhe1011@IWI-lkit3-10>
2018-06-22 15:12:31 +0200

    Aufgabe 1 fertig

--- lab1/src/main/java/de/hska/iwi/bdelab/batchstore/Batchloader.java
+++ lab1/src/main/java/de/hska/iwi/bdelab/batchstore/Batchloader.java
@@ -8,18 +8,32 @@ import java.nio.file.Paths;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
-import de.hska.iwi.bdelab.schema.Data;
+import de.hska.iwi.bdelab.schema.*;
+import manning.tap.DataPailStructure;
+
 import org.apache.hadoop.fs.FileSystem;
+
+import com.backtype.hadoop.pail.Pail;
 
 public class Batchloader {
 
     // ...
+	private Pail<Data> tmpPail;
+	private Pail<Data> masterPail;
+	Pail<Data>.TypedRecordOutputStream os;
 
     private void readPageviewsAsStream() {
         try {
             URI uri = Batchloader.class.getClassLoader().getResource("pageviews.txt").toURI();
             try (Stream<String> stream = Files.lines(Paths.get(uri))) {
-                stream.forEach(line -> writeToPail(getDatafromString(line)));
+                stream.forEach(line -> {
+					try {
+						writeToPail(getDatafromString(line));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				});
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -30,7 +44,8 @@ public class Batchloader {
 
     private Data getDatafromString(String pageview) {
         Data result = null;
-
+        // change this to "true" if you want to work
+        // on the local machines' file system instead of hdfs
         StringTokenizer tokenizer = new StringTokenizer(pageview);
         String ip = tokenizer.nextToken();
         String url = tokenizer.nextToken();
@@ -39,12 +54,32 @@ public class Batchloader {
         System.out.println(ip + " " + url + " " + time);
 
         // ... create Data
+        UserID uid1 = new UserID();
+		uid1.set_user_id(ip);
+
+		PageID pg1 = new PageID();
+		pg1.set_url(url);
+
+		PageViewEdge pve1 = new PageViewEdge();
+		pve1.set_person(uid1);
+		pve1.set_page(pg1);
+		pve1.set_nonce(Integer.parseInt(time));
+
+		DataUnit du1 = new DataUnit();
+		du1.set_page_view(pve1);
+
+		Pedigree pd1 = new Pedigree();
+		pd1.set_true_as_of_secs((int) (System.currentTimeMillis() / 1000));
+		result = new Data();
+		result.set_dataunit(du1);
+		result.set_pedigree(pd1);
 
         return result;
     }
 
-    private void writeToPail(Data data) {
+    private void writeToPail(Data data) throws IOException {
         // ...
+    	os.writeObjects(data);
     }
 
     private void importPageviews() {
@@ -65,13 +100,17 @@ public class Batchloader {
 
             // set up new pail and a stream
             // ...
-
+            tmpPail = Pail.create(fs, newPath, new DataPailStructure());
+            os = tmpPail.openWrite();
             // write facts to new pail
             readPageviewsAsStream();
-
+            
+            os.close();
             // set up master pail and absorb new pail
             // ...
-
+            masterPail = Pail.create(fs, masterPath, new DataPailStructure(), false);
+            masterPail.absorb(tmpPail);
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
